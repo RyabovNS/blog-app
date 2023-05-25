acb88658f0be710632505805e6868c8b67d2abd1
Nikita Ryabov <nikita.kimovsk@mail.ru>
2023-05-25 13:53:41 +0300

    components and linter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactsUsComponent } from './pages/contacts-us/contacts-us.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionComponent,
+    ContactsUsComponent,
+    CommentFormComponent,
+    CommentListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
